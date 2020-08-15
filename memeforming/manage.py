4e68a7e10961eff52050c4b1465373375cce4069
Thomas Wang <thomas@teaminkling.com>
2020-08-15 18:31:34 +1000

    Pedantic blanking of some meta files and general meta update

--- memeforming/manage.py
+++ memeforming/manage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+
 import os
 import sys
 
@@ -7,6 +8,7 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'memeforming.settings')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -15,6 +17,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
     execute_from_command_line(sys.argv)
 
 
