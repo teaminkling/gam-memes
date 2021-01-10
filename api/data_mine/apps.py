5b566e938d3700d3bfb2dacfdd15c6abed8a7d7c
Thomas Wang <thomas@teaminkling.com>
2021-01-10 12:13:58 +1100

    Don't ignore important files

---
+++ api/data_mine/apps.py
@@ -0,0 +1,11 @@
+"""Config file for data mining app."""
+
+from django.apps import AppConfig
+
+
+class DataMineConfig(AppConfig):
+    """Configuration class for data mining app."""
+
+    name = "data_mine"
+
+    verbose_name = "Data Mine"
